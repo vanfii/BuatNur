17197c4f394df3e0f258ea92ea66d92f9ed0ae7e
vanfii <71624882+vanfii@users.noreply.github.com>
2025-02-18 16:24:19 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './ring-svgrepo-com.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          kamu ga kangen aku
+        </p>
+        <a
+          className="App-link"
+          href="https://wa.me/085733337537?text=aku kangen kamu "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          pencet iniii
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
